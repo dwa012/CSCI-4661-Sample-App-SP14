db95735b063acbcda19e3e6761b1997b630aeb78
Daniel Ward <dwa012@gmail.com>
2014-01-22 17:59:13 -0600

    Initial Commit

---
+++ Grocery List/UNOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UNOAppDelegate.h
+//  Grocery List
+//
+//  Created by Daniel Ward on 1/22/14.
+//  Copyright (c) 2014 UNO CSCI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UNOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Grocery List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Grocery List
+//
+//  Created by Daniel Ward on 1/22/14.
+//  Copyright (c) 2014 UNO CSCI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UNOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UNOAppDelegate class]));
+    }
+}
